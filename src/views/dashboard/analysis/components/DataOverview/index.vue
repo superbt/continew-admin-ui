8ac97f3ed6985534e5c9f8f2e06d1be94a07ee1d
bt <dys456123@163.com>
2024-12-15 18:38:52 +0800

    注释3

--- src/views/dashboard/analysis/components/DataOverview/index.vue
+++ src/views/dashboard/analysis/components/DataOverview/index.vue
@@ -13,6 +13,9 @@
       <a-grid-item :span="{ xs: 24, sm: 24, md: 24, lg: 24, xl: 6, xxl: 6 }">
         <Demo2 />
       </a-grid-item>
+      <a-grid-item :span="{ xs: 24, sm: 24, md: 24, lg: 24, xl: 6, xxl: 6 }">
+        <Demo3 />
+      </a-grid-item>
     </a-grid>
     <template #extra>
       <slot name="extra"></slot>
@@ -25,6 +28,7 @@ import Pv from './Pv.vue'
 import Ip from './Ip.vue'
 import Demo1 from './Demo1.vue'
 import Demo2 from './Demo2.vue'
+import Demo3 from './Demo3.vue'
 </script>
 
 <style scoped lang="less"></style>
